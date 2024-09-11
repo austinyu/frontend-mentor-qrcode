fc2fe266792d88c3be46b38ed1f84e778639862c
Sir Austin <42458827+austinyu@users.noreply.github.com>
2024-09-10 21:29:58 -0700

    initial design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      fontSize: {
+        'f-paragraph': '15px'
+      },
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        slate: {
+          300: 'hsl(212, 45%, 89%)',
+          500: 'hsl(216, 15%, 48%)',
+          900: 'hsl(218, 44%, 22%)'
+        }      
+      },
+      borderRadius: {
+        '10': '10px',
+        '20': '20px',
+      }
+    },
   },
   plugins: [],
 }
